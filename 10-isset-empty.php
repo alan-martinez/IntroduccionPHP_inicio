a9ce166159a01d9e5752c2095d05f7b165629bae
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-16 14:57:06 -0500

    10 y 11

--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,22 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes = array();
+$clientes3 = array('Pedro', 'Juan', 'Maria');
 
+//Empy - revisa si un arreglo esta vacio
+var_dump (empty($clientes));
+var_dump (empty($clientes3));
+var_dump (empty($clientes2));
 
+//Isset - revisa si un arreglo ya ha sido creado o una propiedad esta definifa
+echo "<br>";
+var_dump ( isset($clientes4));
+echo "<br>";
+var_dump ( isset($clientes));
+echo "<br>";
+var_dump ( isset($clientes2));
+echo "<br>";
+var_dump ( isset($clientes3));
 
 include 'includes/footer.php';
--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -1,7 +1,38 @@
 <?php include 'includes/header.php';
 
+//in_array - revisa si un valor existe en un arreglo
+$carrito = ['Producto 1', 'Producto 2', 'Producto 3'];
 
+var_dump( in_array('Producto 1', $carrito));
+echo '<br>';
+var_dump( in_array('Producto 4', $carrito));
+echo '<br>';
 
+//Ordenar elementos de un arreglo 
+$numeros = array(1,4,2,3,8);
+sort($numeros); //de menor a mayor
+rsort($numeros); //de mayor a menor
+
+echo '<pre>';
+var_dump($numeros);
+echo '</pre>';
+
+//ordenar arreglo asociativo     
+$cliente = array(
+    'saldo' => 100,
+    'tipo' => 'premium',
+    'nombre' => 'Juan'
+);
+
+asort($cliente); //ordena por valores
+ksort($cliente); //ordena por llaves
+krsort($cliente); //ordena por llaves (de la z a la a)
+asort($cliente); //ordena por valores (de menor a mayor las letras)
+arsort($cliente); //ordena por valores (de mayor a menos las letras)
+
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
 
 
 include 'includes/footer.php';
