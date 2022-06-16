c65ec4eb29234d4a3a6458954842acc72009d192
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-16 15:48:02 -0500

    Condicionales incompleto

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,35 @@
 <?php include 'includes/header.php';
 
+$autenticado = False;
+$admin = True;
 
+if ($autenticado || $admin) {
+    echo 'Autenticado';
+}else {
+    echo 'No autenticado';
+}
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 300,
+    'pedidos' => [
+        'pedido1' => 'Arroz',
+        'pedido2' => 'Pollo',
+        'pedido3' => 'Carne',
+        'pedido4' => 'Pescado'
+    ]
+];
 
+if (empty($cliente)){
+    echo 'El cliente no tiene pedidos';
+}else{
+    echo 'El cliente tiene pedidos';
+
+    if ($cliente['saldo'] > 0){
+        echo 'El cliente tiene saldo';
+    }else{
+        echo 'El cliente no tiene saldo';
+    }
+}
 
 include 'includes/footer.php';
