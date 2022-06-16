b5934bd5976ea420d655545ed07108b2673a185b
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-16 14:32:07 -0500

    Arreglos

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Smartphone', 'Laptop'];
 
+echo "<pre>";
+var_dump($carrito);
+echo "<pre>";
 
+echo $carrito[1];
 
+$carrito[3] = 'Nuevo producto';
+
+echo "<pre>";
+var_dump($carrito);
+echo "<pre>";
+
+//Añadir elemento al final
+array_push($carrito, 'Nuevo producto2');
+
+echo "<pre>";
+var_dump($carrito);
+echo "<pre>";
+
+//Añadir al inicio del
+array_unshift($carrito, 'Nuevo producto3');
+
+echo "<pre>";
+var_dump($carrito);
+echo "<pre>";
 include 'includes/footer.php';
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 300,
+    'pedidos' => [
+        'pedido1' => 'Arroz',
+        'pedido2' => 'Pollo',
+        'pedido3' => 'Carne',
+        'pedido4' => 'Pescado'
+    ]
+];
 
+echo '<pre>';
+var_dump($cliente['nombre']);
+echo '<pre>';
+var_dump($cliente['saldo']);
+echo '<pre>';
+var_dump($cliente['pedidos']['pedido1']);
+echo '<pre>';
+
+$cliente['codigo'] = 124654;
+var_dump($cliente);
+echo '<pre>';
 
 
 include 'includes/footer.php';
